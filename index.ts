bfe529795fd8b9b39e73fec10d4fd754d400ca89
shyMang0 <carinodrex@gmail.com>
2024-03-26 16:37:38 +0800

    working s3 evb to sqs

--- index.ts
+++ index.ts
@@ -2,23 +2,8 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as apigateway from "@pulumi/aws-apigateway";
 
-// A Lambda function to invoke
-const fn = new aws.lambda.CallbackFunction("fn", {
-    callback: async (ev, ctx) => {
-        return {
-            statusCode: 200,
-            body: new Date().toISOString(),
-        };
-    }
-})
+const bucket = new aws.s3.Bucket("bucket");
 
-// A REST API to route requests to HTML content and the Lambda function
-const api = new apigateway.RestAPI("api", {
-    routes: [
-        { path: "/", localPath: "www"},
-        { path: "/date", method: "GET", eventHandler: fn },
-    ]
-});
 
 // The URL at which the REST API will be served.
-export const url = api.url;
+export const url = bucket.arn;
