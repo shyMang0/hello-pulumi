edc752d39ba461dfb0fb0f29670b78ac538cfde0
shyMang0 <carinodrex@gmail.com>
2024-04-19 15:01:10 +0800

    lambda sns refacto

--- Lambda/lambdaStream/index.ts
+++ Lambda/lambdaStream/index.ts
@@ -1,24 +1,26 @@
-import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+import { SNSClient, PublishCommand, PublishCommandInput } from "@aws-sdk/client-sns";
 import { DynamoDBStreamEvent } from "aws-lambda";
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<any> => {
     console.log("lambdaStream Data :", event.Records.length, JSON.stringify(event, null, 2));
     const TopicArn = process.env.TOPIC_ARN;
-    const sns = new SNSClient();
 
     const records = event.Records;
     let filenames = records.map((record) => record.dynamodb?.NewImage?.filename.S);
 
     try {
-        await sns.send(
-            new PublishCommand({
-                Subject: `SNS from Lambda with Stream Origin`,
-                Message: `body => filenames : ${event.Records.length} - ${JSON.stringify(filenames)}`,
-                TopicArn: TopicArn,
-            })
-        );
+        await snsPublish({
+            Subject: `SNS from Lambda with Stream Origin`,
+            Message: `body => filenames : ${event.Records.length} - ${JSON.stringify(filenames)}`,
+            TopicArn: TopicArn,
+        });
         console.log("Message published successfully");
     } catch (error) {
         console.error("Failed to publish message", error);
     }
 };
+
+async function snsPublish(publishCmd: PublishCommandInput) {
+    const sns = new SNSClient();
+    await sns.send(new PublishCommand(publishCmd));
+}
